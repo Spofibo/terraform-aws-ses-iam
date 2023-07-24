8a4f6ad8ff7278a2459d96a8b17c1eb7d11c6ff9
Alex Budurovici <673081+w0rldart@users.noreply.github.com>
2023-07-24 08:26:16 +0200

    Bind SES bounce events to SNS bounce topic

--- ses.tf
+++ ses.tf
@@ -1,3 +1,18 @@
 resource "aws_ses_domain_identity" "this" {
   domain = var.domain_name
 }
+
+resource "aws_ses_configuration_set" "bounce" {
+  name = "${var.name_prefix}-bounce-configuration"
+}
+
+resource "aws_ses_event_destination" "bounce" {
+  name                   = "${var.name_prefix}-bounce-sns"
+  configuration_set_name = aws_ses_configuration_set.bounce.name
+  enabled                = true
+  matching_types         = ["bounce"]
+
+  sns_destination {
+    topic_arn = aws_sns_topic.bounce_notifications[0].arn
+  }
+}
